845163e6dc9dcdca725b00d7e24554892aad96a6
AjajRana <aj.rajput0003@gmail.com>
2024-02-23 09:40:40 +0530

    first commit

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "JavaScript Tutorial",
+    productNumbers: "85743",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    productName: "CSS Full Course",
+    productNumbers: "97245",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    productName: "Flex-Box Tutorial",
+    productNumbers: "36452",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
